6bf36ebd0e261ff0021e4ac49dccb44b5404dd50
Le Dam Duy Phuc <somdp2907@gmail.com>
2023-09-04 15:14:20 +0700

    feat: setup react-router-dom and mui

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import { FC } from "react";
+
+const HomePage: FC = () => {
+  return (
+    <section>
+      <h1>Home Page</h1>
+      <Button>Mui Btn</Button>
+    </section>
+  );
+};
+
+export default HomePage;
